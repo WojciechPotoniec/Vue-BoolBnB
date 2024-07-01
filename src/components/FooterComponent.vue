9076937df03f199bd5ba8e2925581125c628270d
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-07-01 11:32:03 +0200

    created FooterComponent

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
+</style>
