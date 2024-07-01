45e6818db6733e31227b0a4adecbc4ced1ed00f8
itsjackie00 <jackiedanso33@gmail.com>
2024-07-01 18:48:32 +0200

    imported header and jumbo component

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,13 +1,16 @@
 <template>
     <HeaderComponent />
+    <JumboComponent />
 </template>
 
 <script>
 import HeaderComponent from "../components/HeaderComponent.vue";
+import JumboComponent from "../components/JumboComponent.vue";
 export default {
   name: "AppHome",
   components: {
     HeaderComponent,
+    JumboComponent,
   },
 };
 </script>
