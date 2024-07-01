d4ee906bee3cd2ea1de3a62ebb25968336e2bb3e
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-07-01 11:31:24 +0200

    Created HeaderComponent with links

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,58 @@
+<template>
+  HEADER
+  <ul>
+    <li v-for="(link, index) in links" :key="index">
+      <router-link
+        :to="{ name: link.routeName }"
+        class="nav-link"
+        active-class="active"
+      >
+        {{ link.label }}
+      </router-link>
+    </li>
+    <li class="ms-auto">
+      <a href="http://localhost:8000/login" class="nav-link">
+        <span>Login</span>
+      </a>
+    </li>
+    <li class="ms-auto">
+      <a href="http://localhost:8000/register" class="nav-link">
+        <span>Registrati</span>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  data() {
+    return {
+      links: [
+        {
+          label: "Home",
+          routeName: "home",
+        },
+        {
+          label: "Contattaci",
+          routeName: "contact",
+        },
+        {
+          label: "Chi Siamo",
+          routeName: "about",
+        },
+        // {
+        //   label: "Login",
+        //   routeName: "login",
+        // },
+        // {
+        //   label: "Registrati",
+        //   routeName: "register",
+        // },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
