a33ae0ac3a479924b8a558156a4533a69f37a259
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-07-02 13:36:00 +0200

    imported CardComponent to main

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,15 +1,18 @@
 <template>
     <main>
         TEST SLIDER
+        <CardComponent />
         <SliderComponent />
     </main>
 </template>
 
 <script>
+import CardComponent from './CardComponent.vue';
 import SliderComponent from './SliderComponent.vue';
     export default {
         name: 'MainComponent',
         components: {
+            CardComponent,
             SliderComponent
         }
     }
