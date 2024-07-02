518360c17bc93d07e868debb1680d3aa0169c2d5
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-07-02 16:17:29 +0200

    minor fix

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -4,18 +4,13 @@
 </template>
 
 <script>
-import HeaderComponent from "../components/HeaderComponent.vue";
 import JumboComponent from "../components/JumboComponent.vue";
 import MainComponent from "../components/MainComponent.vue";
-import FooterComponent from "../components/FooterComponent.vue";
-
 export default {
   name: "AppHome",
   components: {
-    HeaderComponent,
     MainComponent,
     JumboComponent,
-    FooterComponent,
   },
 };
 </script>
