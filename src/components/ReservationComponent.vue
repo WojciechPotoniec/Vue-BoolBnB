cc8201c7082be8d1dfa93cc0d64463c26fd72444
itsjackie00 <jackiedanso33@gmail.com>
2024-07-03 12:05:16 +0200

    small fix

--- src/components/ReservationComponent.vue
+++ src/components/ReservationComponent.vue
@@ -7,22 +7,22 @@
         <div id="reservation-form" class="d-flex mb-4">
             <input type="date" class="form-select">
             <input type="date" class="form-select">
-                <select name="Ospiti" id="" class="form-select w-100">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                </select>
+            <select name="Ospiti" id="" class="form-select w-100">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+            </select>
         </div>
         <button class="w-100 btn btn-primary">Prenota</button>
         <p class="text-center mt-3">Non puoi modificare questa prenotazione</p>
         <div id="reservation-info">
             <div class="d-flex justify-content-between">
                 <div class="text-decoration-underline m-2">50€ x notte</div>
-                <div >75€</div>
+                <div>75€</div>
             </div>
             <div class="d-flex justify-content-between">
                 <div class="text-decoration-underline m-2">Costi di servizio</div>
@@ -36,23 +36,25 @@
                 <div class="text-decoration-underline m-2">Totale</div>
                 <div>125€</div>
             </div>
-
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'ReservationComponent',
-    }
+export default {
+    name: 'ReservationComponent',
+}
 </script>
 
 <style lang="scss" scoped>
 @use "../assets/styles/partials/variables" as *;
+
 #reservation-container {
     border: 3px solid $secondary-gold;
     border-radius: 15px;
     padding: 1rem;
-    
+    width: 350px;
+
+
 }
 </style>
