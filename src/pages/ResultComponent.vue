9553bf5da963872c85387d308f022730e9d03ec8
itsjackie00 <jackiedanso33@gmail.com>
2024-07-03 16:38:09 +0200

    created result page and added card component in the importation to show the appartment cards

---
+++ src/pages/ResultComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="mt-5 pt-5 container" id="results">
+        <h1 class="mt-2">Results</h1>
+        
+        <CardComponent />
+    </div>
+</template>
+
+<script>
+import CardComponent from '@/components/CardComponent.vue';
+
+    export default {
+        name: 'ResultComponent',
+        components: {
+            CardComponent
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
