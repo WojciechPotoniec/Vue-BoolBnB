12306a7783058255e8988c5bc42429926cf45d29
itsjackie00 <jackiedanso33@gmail.com>
2024-07-03 16:37:36 +0200

    imported router

--- src/components/SearchbardComponent.vue
+++ src/components/SearchbardComponent.vue
@@ -1,34 +1,35 @@
 <template>
     <div id="searchbar" class="mt-3">
-        <div class="d-flex container justify-content-between pt-2">
-            <div id="searchbar_input" class="px-3">
-                <label for="destination">Dove</label>
-                <input type="text" aria-label="Destination" class="form-control" placeholder="Cerca Destinazione"
-                    v-model="searchQuery.destination">
-            </div>
-            <div>
-                <label for="checkin">Data</label>
-                <input type="date" class="form-control" placeholder="Aggiungi data" v-model="searchQuery.checkin">
-            </div>
-            <div>
-                <label for="checkout">Data</label>
-                <input type="date" class="form-control" placeholder="Aggiungi data" v-model="searchQuery.checkout">
-            </div>
-            <div>
-                <label for="guests">Ospiti</label>
-                <input type="text" aria-label="Guests" class="form-control" placeholder="Aggiungi ospiti"
-                    v-model="searchQuery.guests">
-            </div>
-            <button class="btn" @click="search"> <!-- Aggiungi l'evento @click per il bottone -->
-                <i class="fa-solid fa-magnifying-glass"></i>
-            </button>
+      <div class="d-flex container justify-content-between pt-2">
+        <div id="searchbar_input" class="px-3">
+          <label for="destination">Dove</label>
+          <input type="text" aria-label="Destination" class="form-control" placeholder="Cerca Destinazione"
+            v-model="searchQuery.destination">
         </div>
+        <div>
+          <label for="checkin">Data Check-in</label>
+          <input type="date" class="form-control" placeholder="Aggiungi data" v-model="searchQuery.checkin">
+        </div>
+        <div>
+          <label for="checkout">Data Check-out</label>
+          <input type="date" class="form-control" placeholder="Aggiungi data" v-model="searchQuery.checkout">
+        </div>
+        <div>
+          <label for="guests">Ospiti</label>
+          <input type="text" aria-label="Guests" class="form-control" placeholder="Aggiungi ospiti"
+            v-model="searchQuery.guests">
+        </div>
+        <button class="btn" @click="search"> <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </div>
     </div>
-</template>
+  </template>
 
 
 <script>
 import { store } from '../store';
+import { router } from '../router';
+
 export default {
   name: 'SearchbarComponent',
   data() {
@@ -36,14 +37,14 @@ export default {
       store: {
         apiKey: store.apiKey
       },
+      router: router,
       searchQuery: {
         destination: '',
         checkin: '',
         checkout: '',
         guests: ''
       },
-      searchResults: [], // Array per memorizzare i risultati della ricerca
- // Sostituisci con la tua chiave API TomTom
+      searchResults: []
     };
   },
   methods: {
@@ -52,20 +53,20 @@ export default {
         alert('Per favore inserisci una destinazione.');
         return;
       }
-      const url = `https://api.tomtom.com/search/2/geocode/${encodeURIComponent(this.searchQuery.destination)}.json?key=${this.store.apiKey}`; 
-      try {
+      // Replace with your API endpoint URL
+      const url = `https://api.tomtom.com/search/2/geocode/${encodeURIComponent(this.searchQuery.destination)}.json?key=${this.store.apiKey}`;      try {
         const response = await fetch(url);
         const data = await response.json();
-        this.searchResults = data.results;
-        console.log('Risultati della ricerca:', this.searchResults);
+        this.searchResults = data.apartments; // Assuming data structure has apartments key
+
+        // Redirect to results page with search results data
+        this.$router.push({ path: '/results', query: this.searchQuery });
       } catch (error) {
         console.error('Errore durante la ricerca:', error);
       }
-    },
-    search() {
-      console.log('User Input:', this.searchQuery);
     }
-  }
+  },
+  
 };
 </script>
 
