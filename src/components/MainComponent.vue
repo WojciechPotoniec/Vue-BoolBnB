e2d1d93bfde649012fbf7f52deba1cef8a34d4d9
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-07-04 19:16:24 +0200

    updated with CardComponent

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,15 +1,44 @@
 <template>
     <main>
-        <CardComponent />
+        <div class="container">
+            <div class="row">
+        <div class="col-12 col-xl-4 col-lg-6" v-for="(apartment, i) in store.apartments" :key="i">
+            <CardComponent :card="apartment" />
+        </div>
+        </div>
+    </div>
     </main>
 </template>
 
 <script>
+import { store } from '../store';
+import axios from 'axios';
 import CardComponent from './CardComponent.vue';
     export default {
         name: 'MainComponent',
         components: {
             CardComponent
+        },
+        data(){
+            return {
+                store
+            }
+        },
+        //**! METODO PER PRENDERE TUTTI GLI APARTMENT CON L'IMMAGINE IN FORMATO ARRAY SPLITTATO */
+        // methods: {
+        //     getAllApartments(){
+        //         axios.get(this.store.apiBaseUrl + '/apartments', { params: this.params }).then((res)=>{
+        //             this.store.apartments = res.data.results.map(apartment => {
+        //                 apartment.image = JSON.parse(apartment.image);
+        //                 return apartment;
+        //             });
+        //             this.params = null;
+        //             console.log(this.store.apartments)
+        //         })
+        //     }
+        // },
+        mounted(){
+            // this.getAllApartments();
         }
     }
 </script>
