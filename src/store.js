4ca8e319f2b1ec349123309357c773e99acf0d45
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-07-04 19:15:03 +0200

    added apartmets array

--- src/store.js
+++ src/store.js
@@ -4,4 +4,5 @@ export const store = reactive({
     apiBaseUrl: 'http://127.0.0.1:8000/api',
     imgBasePath: 'http://127.0.0.1:8000/storage/',
     defaultImg: '', // da definire un'immagine di default
+    apartments: []
 });
