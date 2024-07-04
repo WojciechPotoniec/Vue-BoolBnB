e06b99c440021e041598d80fa3aed0e359f729bc
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-07-04 19:17:14 +0200

    update with sigle show CardComponent

--- src/pages/ResultComponent.vue
+++ src/pages/ResultComponent.vue
@@ -1,22 +1,33 @@
 <template>
-    <div class="mt-5 pt-5 container" id="results">
-        <h1 class="mt-2">Results</h1>
-        
-        <CardComponent />
+  <div class="mt-5 pt-5 container" id="results">
+    <h1 class="mt-2">Results</h1>
+    <div class="row">
+      <div
+        class="col-12 col-xl-4 col-lg-6"
+        v-for="(apartment, i) in store.apartments"
+        :key="i"
+      >
+        <CardComponent :card="apartment" />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import CardComponent from '@/components/CardComponent.vue';
+import { store } from "../store.js";
+import CardComponent from "@/components/CardComponent.vue";
 
-    export default {
-        name: 'ResultComponent',
-        components: {
-            CardComponent
-        }
-    }
+export default {
+  name: "ResultComponent",
+  components: {
+    CardComponent,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
