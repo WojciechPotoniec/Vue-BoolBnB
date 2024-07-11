a73650319601dc05b133287e41d17c242f78dcaa
itsjackie00 <jackiedanso33@gmail.com>
2024-07-11 11:28:58 +0200

    created and added content

---
+++ src/components/GuaranteeComponent.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="guarantee-container" class="container mt-5">
+        <div id="guarantee" class="text-center">
+            <h3>Boolbnb's Guarantee Policy</h3>
+            <p class="w-50 mx-auto">Booking with Boolbnb is easy and secure. We personally verify all properties to
+                ensure that the service provided is always top-notch! <br>
+                <strong>Book today and guarantee yourself a fantastic experience!</strong>
+            </p>
+        </div>
+        <div class="row m-3">
+            <div class="col-12 col-xl-4 col-lg-6  text-center">
+                <i class="fa-solid fa-award"></i>
+                <p>Secure booking
+                <p>Our Booking Guarantee
+                    <br>
+                    Sicuro offers 24/7 support
+                </p>
+                </p>
+            </div>
+            <div class="col-12 col-xl-4 col-lg-6 text-center">
+                <i class="fa-solid fa-suitcase"></i>
+                <p>Better quality of time
+                <p>
+                    From booking to stay, the journey is simple and pleasant
+                </p>
+                </p>
+            </div>
+            <div class="col-12 col-xl-4 col-lg-6 text-center">
+                <i class="fa-solid fa-mug-hot"></i>
+                <p>With the intimacy of a home
+                <p>
+                    All the comforts at your disposal, swimming pool, free parking, Wi-fi, and much more
+                </p>
+                </p>
+            </div>
+            <div class="col-12 col-xl-4 col-lg-6 text-center">
+                <i class="fa-solid fa-piggy-bank"></i>
+                <p>Much more than a holiday home 
+                <p>
+                    More space, more privacy, more services.. better quality
+                </p>
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'GuaranteeComponent',
+}
+</script>
+
+<style lang="scss" scoped>
+#guarantee-container {
+    background-color: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5));
+    padding: 50px 0;
+}
+</style>
