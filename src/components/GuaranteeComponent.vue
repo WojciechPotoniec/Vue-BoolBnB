a69b177736912c077e4ac6b3bb4da4b684105d01
itsjackie00 <jackiedanso33@gmail.com>
2024-07-11 11:35:26 +0200

    minor fix on css

--- src/components/GuaranteeComponent.vue
+++ src/components/GuaranteeComponent.vue
@@ -1,41 +1,41 @@
 <template>
     <div id="guarantee-container" class="container mt-5">
         <div id="guarantee" class="text-center">
-            <h3>Boolbnb's Guarantee Policy</h3>
+            <h3 class="title-guarantee">Boolbnb's Guarantee Policy</h3>
             <p class="w-50 mx-auto">Booking with Boolbnb is easy and secure. We personally verify all properties to
                 ensure that the service provided is always top-notch! <br>
                 <strong>Book today and guarantee yourself a fantastic experience!</strong>
             </p>
         </div>
-        <div class="row m-3">
-            <div class="col-12 col-xl-4 col-lg-6  text-center">
-                <i class="fa-solid fa-award"></i>
-                <p>Secure booking
+        <div class="row mt-5">
+            <div class="col-12 col-xl-3 col-lg-6  text-center">
+                <i class="fa-solid fa-award mb-3"></i>
+                <p class="sub-guarantee">Secure booking
                 <p>Our Booking Guarantee
                     <br>
                     Sicuro offers 24/7 support
                 </p>
                 </p>
             </div>
-            <div class="col-12 col-xl-4 col-lg-6 text-center">
-                <i class="fa-solid fa-suitcase"></i>
-                <p>Better quality of time
+            <div class="col-12 col-xl-3 col-lg-6 text-center">
+                <i class="fa-solid fa-suitcase mb-3"></i>
+                <p class="sub-guarantee">Better quality of time
                 <p>
                     From booking to stay, the journey is simple and pleasant
                 </p>
                 </p>
             </div>
-            <div class="col-12 col-xl-4 col-lg-6 text-center">
-                <i class="fa-solid fa-mug-hot"></i>
-                <p>With the intimacy of a home
+            <div class="col-12 col-xl-3 col-lg-6 text-center">
+                <i class="fa-solid fa-mug-hot mb-3"></i>
+                <p class="sub-guarantee">With the intimacy of a home
                 <p>
                     All the comforts at your disposal, swimming pool, free parking, Wi-fi, and much more
                 </p>
                 </p>
             </div>
-            <div class="col-12 col-xl-4 col-lg-6 text-center">
-                <i class="fa-solid fa-piggy-bank"></i>
-                <p>Much more than a holiday home 
+            <div class="col-12 col-xl-3 col-lg-6 text-center">
+                <i class="fa-solid fa-piggy-bank mb-3"></i>
+                <p class="sub-guarantee">Much more than a holiday home 
                 <p>
                     More space, more privacy, more services.. better quality
                 </p>
@@ -52,8 +52,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
 #guarantee-container {
-    background-color: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5));
     padding: 50px 0;
+}
+
+.title-guarantee{
+    font-size: 30px;
+    font-weight: bold;
+    color: $primary-bg;
+}
+.fa-solid{
+    font-size: 50px;
+    color: $primary-bg;
+}
+.sub-guarantee{
+    font-size: 20px;
+    font-weight: bold;
+    color: $primary-bg;
 }
 </style>
